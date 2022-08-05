d4f3a91fa149c29ea56b6cb4d2cebff66670713f
Devendra Ghormare <devendraghormare4192@gmail.com>
2022-08-05 19:38:33 +0530

    static

--- personalportfolio/urls.py
+++ personalportfolio/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "Personal Portfolio Login"
 admin.site.site_title = "Personal Portfolio Admin Portal"
@@ -24,4 +25,4 @@ admin.site.index_title = "Welcome to Portfolio Database"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
